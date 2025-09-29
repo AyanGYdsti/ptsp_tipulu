5dc069f5e59baf580159575d66d6192183c691a0
IankSyahputra <ihramsyih@gmail.com>
2025-09-30 00:00:34 +0800

    update

--- resources/views/backend/list-pengajuan/index.blade.php
+++ resources/views/backend/list-pengajuan/index.blade.php
@@ -26,7 +26,7 @@
                             <tr class="hover:bg-blue-50 transition">
                                 <td class="px-4 py-3 text-center font-semibold text-blue-600">{{ $loop->iteration }}</td>
                                 <td class="px-4 py-3">{{ $data->masyarakat->nama }}</td>
-                                <td class="px-4 py-3">{{ $data->masyarakat->no_hp }}</td>
+                                <td class="px-4 py-3">{{ $data->no_hp }}</td>
                                 <td class="px-4 py-3">{{ $data->pelayanan->nama }}</td>
                                 <td class="px-4 py-3 text-center flex justify-center gap-3">
                                     @if (!$data->verifikasiByAparatur(4)->first())
