c4355a25246d4e519d1ce76dcdfad951a1042895
IankSyahputra <ihramsyih@gmail.com>
2025-10-06 20:02:30 +0800

    update sejarah

--- resources/views/frontend/sejarah/index.blade.php
+++ resources/views/frontend/sejarah/index.blade.php
@@ -14,7 +14,7 @@
             <div class="bg-white p-6 rounded-2xl shadow-md hover:shadow-xl transition text-center">
                 <h3 class="text-2xl font-bold mb-4">Sejarah Singkat</h3>
                 <p class="text-gray-600 mb-6">
-                    Nama Tipulu berasal dari sebuah pohon besar bernama “Tipulu” yang dahulu tumbuh di wilayah RT 10. Dari pohon inilah ....
+                    Nama Tipulu berasal dari sebuah pohon besar bernama “Tipulu” yang dahulu ....
                 </p>
                 <a href="{{ route('sejarah.detail') }}"
                    class="inline-block px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-green-700 transition">
