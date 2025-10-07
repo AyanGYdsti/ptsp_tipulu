967b1c8e3a4ce2b3b3d999d4745d2b426e6d2d57
AyanGYdsti <yangisna02.com>
2025-10-07 22:11:38 +0800

    fix

--- resources/views/layouts/main_frontend.blade.php
+++ resources/views/layouts/main_frontend.blade.php
@@ -27,8 +27,8 @@
     <meta name="twitter:description" content="Akses layanan administrasi kelurahan secara online dengan cepat dan mudah.">
     <meta name="twitter:image" content="{{ asset('assets/images/thumbnail.png') }}">
 
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="/assets/js/tail.js"></script>
+    <script src="/assets/js/chart.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
